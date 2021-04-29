9dc391499ae0fff19f2914f1415c9a97bfef4190
ahmadhidayah <ahmadhidayah4444@gmail.com>
2021-04-29 20:40:29 +0700

    menambhkan file script.js dan menambahkan typed.js dan aos.js pada file index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+var judul = document.getElementById("#judul");
+var judul = new Typed("#judul", {
+    strings: ["Perumahan \n Masa Kini",],
+    typeSpeed: 100,
+    startDelay: 90,
+    loop: true
+  });
